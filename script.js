66e1598afb34a2a2f128b1bdc464694dee4fbef2
Abdulla Alkhan <b00077888@aus.edu>
2023-05-25 13:11:42 +0300

    hello

---
+++ script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['#081b29', '#4A4D54','#4A4D54','#4A4D54'],
+    navigation: true,
+    slidesNavigation: true,
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
+    navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
+});
