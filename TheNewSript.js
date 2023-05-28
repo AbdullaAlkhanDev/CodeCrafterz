29253a93191470aa0da0af0d57e1cf3dec4f40cd
jbinsanad1 <108614522+jbinsanad1@users.noreply.github.com>
2023-05-28 16:06:55 +0300

    Update TheNewSript.js

--- TheNewSript.js
+++ TheNewSript.js
@@ -7,13 +7,21 @@ new fullpage('#fullpage', {
     slidesNavigation: true,
     anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
     navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
+
 });
 
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle')
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible")
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true)
+    } else {
+        primaryNav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false)
+    }
+});
+
